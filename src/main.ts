6d601675bf484c41d2dee88e3657e829e6860827
nischaygadher15 <nischaygadher15@gmail.com>
2025-12-26 12:43:37 +0530

    cors fix

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // CORS issue
   app.enableCors({
-    origin: ['*'],
+    origin: ['https://gsrtc2025.vercel.app', 'http://localhost:3000'],
     credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
   });
 
   //Global pipe for payload validation
